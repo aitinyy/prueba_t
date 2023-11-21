83c2b5239adcccda306a386cfc4e83e87484283b
aitinyy <aitanassempere@gmail.com>
2023-11-21 20:52:33 +0100

    test

--- index.js
+++ index.js
@@ -63,15 +63,17 @@ restService.post("/echo", function(req, res) {
         "fulfillmentText": speech,
         "fulfillmentMessages": [
           {
-            /*"text": {
+            "text": {
               "text": [speech]
-            },*/
-            "telegram": {
-              "text": "El *acné hormonal* está relacionado con los cambios en los niveles de hormonas. Puede aparecer en distintas etapas de la vida como la adolescencia, la menstruación, durante un embarzo o en la menopausa.",
-              "parse_mode": "Markdown"
             }
           }
-        ],  
+        ], 
+        "payload": {
+          "telegram": {
+            "text": "El *acné hormonal* está relacionado con los cambios en los niveles de hormonas. Puede aparecer en distintas etapas de la vida como la adolescencia, la menstruación, durante un embarzo o en la menopausa.",
+            "parse_mode": "Markdown"
+          }
+        },
         "source": "<webhookpn1>"
         });
       //
